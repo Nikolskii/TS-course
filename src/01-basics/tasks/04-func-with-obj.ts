7a730ee3173a24aef59b7afeebccd1efef3c4ecf
Denis Nikolsky <den-apple@ya.ru>
2023-02-09 21:06:27 +0200

    04-func-with-obj

---
+++ src/01-basics/tasks/04-func-with-obj.ts
@@ -0,0 +1,53 @@
+// Создайте интерфейсы User и Admin.
+
+// User должен содержать следующие ключи:
+
+// login, email, password, isOnline, lastVisited
+// Admin содержит все те же ключи, плюс ключ role.
+
+interface User {
+  login: string;
+  email: string;
+  password: string;
+  isOnline: boolean;
+  lastVisited: Date;
+}
+
+interface Admin {
+  login: string;
+  email: string;
+  password: string;
+  isOnline: boolean;
+  lastVisited: Date;
+  role: string;
+}
+
+const user1: User = {
+  login: 'denis',
+  email: 'some@email.com',
+  password: '12345',
+  isOnline: false,
+  lastVisited: new Date(2022, 6, 2),
+};
+
+const admin1: Admin = {
+  login: 'denis',
+  email: 'some@email.com',
+  password: '12345',
+  isOnline: false,
+  lastVisited: new Date(2022, 6, 2),
+  role: 'admin',
+};
+
+// Создайте функцию login, которая принимает один параметр в виде объекта с логином и паролем.
+
+// Проверьте, что поля не пустые и выведите приветственное сообщение в консоль.
+
+const login = (user: { login: string; password: string }): void => {
+  if (user.login.length > 0 && user.password.length > 0) {
+    console.log('Hello', user.login);
+  }
+};
+
+login(user1);
+login(admin1);
