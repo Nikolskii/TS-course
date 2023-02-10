ed99435469d76e101be6ab0628bcfcd95612e8ea
Denis Nikolsky <den-apple@ya.ru>
2023-02-10 20:51:54 +0200

    06-tuples

---
+++ dist/06-tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+const pairs = [
+    ['key', 'value'],
+    ['key2', 'value2'],
+];
+const data = [1, true, 'lodash'];
+// csv
+const doc = [];
+doc.push(['Denis', 'nik', 5, new Date(2022, 4, 5)]);
---
+++ src/01-basics/06-tuples.ts
@@ -0,0 +1,10 @@
+const pairs: [string, string][] = [
+  ['key', 'value'],
+  ['key2', 'value2'],
+];
+
+const data: [number, boolean, string] = [1, true, 'lodash'];
+
+// csv
+const doc: [string, string, number, Date][] = [];
+doc.push(['Denis', 'nik', 5, new Date(2022, 4, 5)]);
