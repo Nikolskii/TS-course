63f3567dc6e6cc0678b14be68a6ed01866986100
Denis Nikolsky <den-apple@ya.ru>
2023-02-12 21:45:33 +0200

    03-interfaces-vs-aliases

---
+++ dist/02-intermediate/03-interfaces-vs-aliases.js
@@ -0,0 +1,7 @@
+"use strict";
+// Aliases type
+// 1. primitives, union, intersections
+// type Window cant be created
+// Interfaces
+// 1. when we want expand interfaces in future
+// 2. class with implements
