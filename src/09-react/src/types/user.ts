b51fd98f497ae2be85ca09553376ba63705fa771
Denis Nikolsky <den-apple@ya.ru>
2023-02-19 16:39:50 +0200

    Создание типов сущностей

---
+++ src/09-react/src/types/user.ts
@@ -0,0 +1,36 @@
+export type LocalGithubUser = {
+  login: string;
+  id: number;
+  avatar: string;
+  name: string;
+  compamy: string;
+  blog: string;
+  location: string;
+  bio: string;
+  twitter: string;
+  repos: number;
+  followers: number;
+  following: number;
+  created: string;
+};
+
+export type GithubUser = {
+  login: string;
+  id: number;
+  avatar_url: string;
+  name: string;
+  compamy: string;
+  blog: string;
+  location: string;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+};
+
+export type GithubError = {
+  message: string;
+  documentation_url: string;
+};
