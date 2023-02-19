0805623e89ffc22be0de12ca0af4056eccfd08bd
Denis Nikolsky <den-apple@ya.ru>
2023-02-19 11:23:42 +0200

    Типизация стейта

--- src/08-react/src/App.tsx
+++ src/08-react/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
+type ITodo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<string[]>([]);
+  const [] = useState<ITodo | null>(null);
+
   return (
     <div className="App">
       <TodoItem
