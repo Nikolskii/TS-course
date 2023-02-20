53f2e51131ea1f62c655491fe53579bf6db5d76f
Denis <den-apple@ya.ru>
2023-02-20 13:27:16 +0200

    Шапка карточки

--- src/09-react/src/components/UserCard/UserCard.tsx
+++ src/09-react/src/components/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserStat } from 'components/UserStat';
+import { UserTitle } from 'components/UserTitle';
 import { LocalGithubUser } from 'types';
 import styles from './UserCard.module.scss';
 
@@ -6,6 +7,11 @@ interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img src={props.avatar} alt={props.login} className={styles.avatar} />
+    <UserTitle name={props.name} login={props.login} created={props.created} />
+    <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio'}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
