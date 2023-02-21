f23ea2593767170907c2a4285860ceb4311d05c1
Denis Nikolsky <den-apple@ya.ru>
2023-02-21 14:59:08 +0200

    Типизация селекторов

--- src/08-react/src/features/Todo/TodoList.tsx
+++ src/08-react/src/features/Todo/TodoList.tsx
@@ -1,10 +1,13 @@
 import TodoItem from 'components/TodoItem';
+import { useSelector } from 'react-redux';
 import { useAppDispatch, useAppSelector } from 'redux-hook';
 import { Todo } from 'types';
+import { selectAllTodos } from './todoSelectors';
 import { removeTodo, toggleTodo } from './todoSlice';
 
 const TodoList = () => {
-  const list = useAppSelector((state) => state.todos);
+  // const list = useAppSelector((state) => state.todos);
+  const list = useSelector(selectAllTodos);
   const dispatch = useAppDispatch();
 
   const handleRemoveTodo = (id: Todo['id']) => {
---
+++ src/08-react/src/features/Todo/todoSelectors.ts
@@ -0,0 +1,3 @@
+import { RootState } from 'store';
+
+export const selectAllTodos = (state: RootState) => state.todos;
