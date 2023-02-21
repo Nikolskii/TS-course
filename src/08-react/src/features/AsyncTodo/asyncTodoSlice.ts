db86a7ce166c87c760162a68e70ac22693f5ff09
Denis <den-apple@ya.ru>
2023-02-21 16:34:58 +0200

    Типизация extraReducers

--- src/08-react/src/features/AsyncTodo/asyncTodoSlice.ts
+++ src/08-react/src/features/AsyncTodo/asyncTodoSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Todo } from 'types';
+import { createTodo, fetchAllTodos } from './todoAsyncActions';
 
 type TodoSlice = {
   status: 'idle' | 'loading' | 'finished' | 'error';
@@ -13,9 +14,24 @@ const initialState: TodoSlice = {
 
 const todoSlice = createSlice({
   name: '@todos',
-  // initialState: [] as Todo[],
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllTodos.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllTodos.fulfilled, (state, action) => {
+        state.status = 'finished';
+        state.list = action.payload;
+      })
+      .addCase(fetchAllTodos.rejected, (state) => {
+        state.status = 'error';
+      })
+      .addCase(createTodo.fulfilled, (state, action) => {
+        state.list.push(action.payload);
+      });
+  },
 });
 
 export default todoSlice.reducer;
