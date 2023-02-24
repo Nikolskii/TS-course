efe823ff075838a24c131e3bec7966943563cd66
Denis <den-apple@ya.ru>
2023-02-24 10:37:58 +0200

    chore: типизировать Countries

--- src/11-react-redux-ts/src/features/countries/countries-slice.js
+++ src/11-react-redux-ts/src/features/countries/countries-slice.js
@@ -2,27 +2,27 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const loadCountries = createAsyncThunk(
   '@@countries/load-countries',
-  (_, {
-    extra: {client, api},
-  }) => {
-    return client.get(api.ALL_COUNTRIES)
+  (_, { extra: { client, api } }) => {
+    return client.get(api.ALL_COUNTRIES);
   },
   {
     condition: (_, { getState }) => {
-      const { countries: { status } } = getState();
+      const {
+        countries: { status },
+      } = getState();
 
       if (status === 'loading') {
         return false;
       }
-    }
-  }
+    },
+  },
 );
 
 const initialState = {
   status: 'idle',
   error: null,
   list: [],
-}
+};
 
 const countrySlice = createSlice({
   name: '@@countries',
@@ -41,9 +41,9 @@ const countrySlice = createSlice({
       .addCase(loadCountries.fulfilled, (state, action) => {
         state.status = 'received';
         state.list = action.payload.data;
-      })
-  }
-})
+      });
+  },
+});
 
 export const countryReducer = countrySlice.reducer;
 
@@ -51,14 +51,14 @@ export const countryReducer = countrySlice.reducer;
 export const selectCountriesInfo = (state) => ({
   status: state.countries.status,
   error: state.countries.error,
-  qty: state.countries.list.length
-})
+  qty: state.countries.list.length,
+});
 
 export const selectAllCountries = (state) => state.countries.list;
-export const selectVisibleCountries = (state, {search = '', region = ''}) => {
+export const selectVisibleCountries = (state, { search = '', region = '' }) => {
   return state.countries.list.filter(
-    country => (
-      country.name.toLowerCase().includes(search.toLowerCase()) && country.region.includes(region)
-    )
-  )
-}
+    (country) =>
+      country.name.toLowerCase().includes(search.toLowerCase()) &&
+      country.region.includes(region),
+  );
+};
--- src/11-react-redux-ts/src/features/countries/use-countries.js
+++ src/11-react-redux-ts/src/features/countries/use-countries.js
@@ -1,14 +1,20 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import { selectControls } from '../controls/controls-slice';
-import { loadCountries, selectCountriesInfo, selectVisibleCountries } from './countries-slice';
+import {
+  loadCountries,
+  selectCountriesInfo,
+  selectVisibleCountries,
+} from './countries-slice';
 
 export const useCountries = () => {
   const dispatch = useDispatch();
   const controls = useSelector(selectControls);
-  const countries = useSelector(state => selectVisibleCountries(state, controls));
-  const {status, error, qty} = useSelector(selectCountriesInfo);
+  const countries = useSelector((state) =>
+    selectVisibleCountries(state, controls),
+  );
+  const { status, error, qty } = useSelector(selectCountriesInfo);
 
   useEffect(() => {
     if (!qty) {
@@ -16,5 +22,5 @@ export const useCountries = () => {
     }
   }, [qty, dispatch]);
 
-  return [countries, {status, error, qty}];
-}
+  return [countries, { status, error, qty }];
+};
